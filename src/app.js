89f88636778140b98a5e28233fed7c58ef0ee363
bastianmoral <bastihc@gmail.com>
2021-10-02 14:16:49 -0300

    PRIMER UPLOAD RCG

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +6,56 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //write your code here.
+}*/
+function deckbuilder() {
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["Hearts", "Diams", "Spades", "Clubs"];
+  const cards = [];
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      const value = values[v];
+      const suit = suits[s];
+      cards.push({ value, suit });
+    }
+  }
+  return cards;
+}
+function randomcard(cards) {
+  let random = Math.floor(Math.random() * 51);
+  let cardValue = cards[random].value;
+  let cardSuit = cards[random].suit;
+  let entity = "&" + cardSuit.toLowerCase() + ";";
+  const card = document.createElement("div");
+  card.classList.add("card", cardSuit.toLowerCase());
+  card.innerHTML =
+    '<span class="card-value-suit top">' +
+    cardValue +
+    entity +
+    "</span>" +
+    '<span class="card-suit">' +
+    entity +
+    "</span>" +
+    '<span class="card-value-suit bot">' +
+    cardValue +
+    entity +
+    "</span>";
+  document.body.appendChild(card);
+}
+
+let cards = deckbuilder();
+randomcard(cards);
